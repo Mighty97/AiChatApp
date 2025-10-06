89fa02dcbab4a110514d181da574d9a6f4ec1c67
Mighty <realofficialvictor@gmail.com>
2025-10-06 05:07:33 +0100

    guard operator to determine which profile image to show based on sender

--- src/component/ChatMessage.jsx
+++ src/component/ChatMessage.jsx
@@ -1,10 +1,12 @@
 import UserProfileImage from '../assets/user.jpg'
+import RobotProfileImage from '../assets/robot.jpg'
 
 export function ChatMessage({message, sender}) {
   return (
     <div>
+      {sender ==='robot' && <img src={RobotProfileImage} alt="" width={45} /> }
       {message}
-      <img src={UserProfileImage} width={45} alt="" />
+      {sender ==='user' &&       <img src={UserProfileImage} width={45} alt="" />}
     </div>
   )
 }
