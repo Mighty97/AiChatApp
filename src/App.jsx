26d8ffa166739d00073d3bc1d235979d75143679
Mighty <realofficialvictor@gmail.com>
2025-10-08 03:47:32 +0100

    starting ChatMessages allover again

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { ChatInput } from './component/ChatInput'
-import { ChatMessage,chatMessages } from './component/ChatMessage'
 import './App.css'
 
 function App() {
   return (
     <>
-      {chatMessages.map((chatMessage) => {
-    return (
-      <ChatMessage message={chatMessage.message} sender={chatMessage.sender} key={chatMessage.id} />
-    )
-  })}
-      <ChatInput />
+    
+    <ChatInput />
     </>
   )
 }
--- src/component/ChatInput.jsx
+++ src/component/ChatInput.jsx
@@ -1,19 +1,10 @@
-import { chatMessages } from "./ChatMessage";
+
 
 export function ChatInput() {
-  function sendMessage() {
-    console.log(chatMessages);
-    chatMessages.push({
-      message: 'testing push on add button',
-      sender: 'user',
-      id: crypto.randomUUID()
-    });
-    console.log(chatMessages);
-  }
   return (
     <>
       <input type="text" placeholder="Send a message to Chatbot..." />
-      <button onClick={sendMessage} >Send</button>
+      <button>Send</button>
     </>
   )
 }
